4e8afe4146ca27c3ed2ec6879803bf0f4f8bff78
doosemavis <moosedavis2011@gmail.com>
2020-09-23 15:10:01 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,25 @@
+class Artist
+
+    attr_accessor :name 
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end 
+
+    def self.all
+        @@all 
+    end 
+
+    def songs
+    end 
+
+    def new_song
+    end 
+
+    def genres
+    end 
+
+end 
